5ad5ab38e55f01d33e9b24401a7d020acc55cd79
Dezmonoid <Dezmoonoid@mail.ru>
2024-01-06 17:27:20 +0300

    Начальная форма.

--- app/src/main/java/com/example/experement_android/Authorization.kt
+++ app/src/main/java/com/example/experement_android/Authorization.kt
@@ -3,9 +3,9 @@ package com.example.experement_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Authorization : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.authorization_main)
     }
 }
